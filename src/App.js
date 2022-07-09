4d88403e75395fc2298dfb576b6ba61dfba80bd7
unknown <ososamuel035@gmail.com>
2022-07-09 18:31:35 +0100

    ScrollToTop component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home/Home";
 import About from "./About/About";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
+import ScrollToTop from "./ScrollToTop/ScrollToTop";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <div className="noise"></div>
         <div className="App">
+          <ScrollToTop />
           <Switch>
             <Route path="/" exact>
               <Home />
